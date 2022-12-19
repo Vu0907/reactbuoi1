c997ee6ca274cec277072dba2ea4cb32fdc27b30
Vu0907 <hxv0907@gmail.com>
2022-12-19 11:04:23 +0700

    react1

---
+++ src/BaiTapLayoutComponent/body.js
@@ -0,0 +1,11 @@
+import Banner from "./banner";
+import Item from "./item";
+
+export default function Body() {
+  return (
+    <div>
+      <Banner />
+      <Item />
+    </div>
+  );
+}
---
+++ src/BaiTapLayoutComponent/index.js
@@ -0,0 +1,13 @@
+import Header from "./header";
+import Body from "./body";
+import Footer from "./footer";
+
+export default function BaitapLayoutComponent() {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+}
